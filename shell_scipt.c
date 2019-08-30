85eb6b8eec5c600e0a06f2000392ab70864e8d19
samarth1107 <samarth1107@gmail.com>
2019-08-31 03:22:46 +0530

    option added

--- shell_scipt.c
+++ shell_scipt.c
@@ -12,27 +12,55 @@
 #include<sys/wait.h> 
 #include "/usr/include/readline/readline.h"
 
+
+//prototype of functions
+void welcome_message();
+void ending_message();
+int takeInput(char*);
+void show_directory();
+void Command_handler(char**,int);
+int count_word(char*,char**);
+void in_echo(char**,int);
+void in_pwd(char**,int);
+void ext_cat(char**,int);
+void ext_date(char**,int);
+void ext_ls(char**,int);
+void ext_rm(char**,int);
+void ext_mkdir(char**,int);
+void add_in_history(char*);
+void show_history(char**,int);
+
 //welcome message
 void welcome_message() 
 { 
 	system("clear");
 	printf("\n\n\n\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"); 
-	printf("\n------------------------------Welcome to Samarth's SHELL-------------"); 
+	printf("\n---------------------------Welcome to SAM SHELL----------------------"); 
 	printf("\n_____________________________________________________________________"); 
 	printf("\n\n\nEntering shell as : @%s user", getenv("USER")); 
 	printf("\nEnter you command when you see '------->>>'  to exit from shell enter 'exit' ");
+	printf("\n "); 
+} 
+
+//ending message
+void ending_message() 
+{ 
+	printf("\n\n\n\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"); 
+	printf("\n---------------------Thank you for using SAM Shell-------------------"); 
+	printf("\n_____________________________________________________________________\n"); 
 	printf("\n"); 
 } 
 
 //this function will take input
 int takeInput(char* input_str) 
 { 
-	char* temp=readline("\n------->>> ");
+	char* temp=readline("------->>> ");
+	//scanf("\n------->>> %s",temp);
 	if (strlen(temp)!=0) 
-	{
+	{		
 		strcpy(input_str,temp); 
 		return 0; 
-	} 
+	} 	
 	return 1;
 } 
 
@@ -40,11 +68,11 @@ int takeInput(char* input_str)
 void show_directory() 
 { 
 	char pwd[1024]; 
-	printf("\nDirectory is ==> %s", getcwd(pwd, sizeof(pwd))); 
+	printf("\nDirectory is ==> %s\n", getcwd(pwd, sizeof(pwd))); 
 }
 
 //this function will call correcsponding function related to commands
-void Command_handler(char** parsed,int no_of_words) 
+void Command_handler(char** user_input,int no_of_words) 
 { 
 	int i=0, choice = 0; 
 	char* list_of_cmd[10]; 	
@@ -62,7 +90,7 @@ void Command_handler(char** parsed,int no_of_words)
 
 	while (i<10) 
 	{ 
-		if (strcmp(parsed[0], list_of_cmd[i]) == 0) 
+		if (strcmp(user_input[0], list_of_cmd[i]) == 0) 
 		{ 
 			choice = i + 1; 
 			break; 
@@ -73,64 +101,64 @@ void Command_handler(char** parsed,int no_of_words)
 
 	if(choice==1)
 	{
-		printf("\nThank you for using my shell\nSee you again\n"); 
+		ending_message(); 
 		exit(0);
 	}
 	else if(choice==2)
 	{
-		chdir(parsed[1]); 
+		chdir(user_input[1]); 
 		show_directory();
 	}
 	else if(choice==3)
 	{
-		in_pwd(parsed,no_of_words);
+		in_pwd(user_input,no_of_words);
 	}
 	else if(choice==4)
 	{
-		show_history();		
+		show_history(user_input,no_of_words);		
 	}
 	else if(choice==5)
 	{
-		in_echo(parsed,no_of_words);
+		in_echo(user_input,no_of_words);
 	}
 	else if(choice==6)
 	{
-		ext_cat(parsed,no_of_words);
+		ext_cat(user_input,no_of_words);
 	}
 	else if(choice==7)
 	{	
-		ext_date(parsed,no_of_words);
+		ext_date(user_input,no_of_words);
 	}
 	else if(choice==8)
 	{
-		ext_ls(parsed,no_of_words);
+		ext_ls(user_input,no_of_words);
 	}
 	else if(choice==9)
 	{
-		ext_rm(parsed,no_of_words);
+		ext_rm(user_input,no_of_words);
 	}
 	else if(choice==10)
 	{	
-		ext_mkdir(parsed,no_of_words);
+		ext_mkdir(user_input,no_of_words);
 	}
 	else
 	{
-		printf("\nthis command is out of scope\n");
+		printf("\nThis command is out of scope of the shell or maybe an invalid command ");
+		printf("\n Don't worry you can try other command");
 	}
 } 
 
-
 //this will count number of words in string
-int count_word(char* str, char** parsed) 
+int count_word(char* str, char** user_input) 
 { 
 	int i=0; 
 	while(i<100) 
 	{ 
-		parsed[i] = strsep(&str," "); 
+		user_input[i] = strsep(&str," "); 
 
-		if (parsed[i] == NULL) 
+		if (user_input[i] == NULL) 
 			break; 
-		if (strlen(parsed[i]) == 0) 
+		if (strlen(user_input[i]) == 0) 
 			i--; 
 		i++;
 	} 
@@ -138,7 +166,7 @@ int count_word(char* str, char** parsed)
 } 
 
 //this will work for for echo command
-void in_echo(char** text,int word_cound)
+void in_echo(char** text,int word_count)
 {
 	pid_t process_id = fork();  
   
@@ -150,9 +178,38 @@ void in_echo(char** text,int word_cound)
 	
 	else if (process_id == 0) 
 	{   
-		if(word_cound>1)for(int i=1;i<word_cound;i++)printf("%s ",text[i]);
+		
+		if (word_count>1)
+		{
+			int option=0;
+			for(int i=0;i<word_count;i++)
+			{
+				if (strcmp(text[i],"-n")==0)
+				{
+					option=1;
+					break;
+				}
+				else if (strcmp(text[i],"-E")==0)
+				{
+					option=2;
+					break;
+				}
+			}
 
-		else printf("Error : Enter what to print");
+			if (option==0)
+			{
+				for(int i=1;i<word_count;i++)printf("%s ",text[i]);
+				printf("\n");
+			}
+			else if(option==1)for(int i=2;i<word_count;i++)printf("%s ",text[i]);
+			else if(option==2)
+			{
+				for(int i=2;i<word_count;i++)printf("%s ",text[i]);
+				printf("\n");
+			}
+		}
+
+		else printf("\n");
 
 		exit(0);
     } 
@@ -169,14 +226,18 @@ void in_pwd(char** argruments,int word_count)
 {
 	char pwd[200];
 
-	if (word_count>1)
+	if (word_count>2)printf("Error: pwd accepts only one argrument");
+
+	else if (word_count>1)
 		{
-			if (strcmp(argruments[1],"-L")==0)printf("\nYou are working in the -->>> %s logical directory",getcwd(pwd, sizeof(pwd)));
-			else if (strcmp(argruments[1],"-P")==0)printf("\nYou are working in the -->>> %s physical directory",getcwd(pwd, sizeof(pwd)));
-			else printf("\nError : incorrect option is enterterd");
+			if (strcmp(argruments[1],"-L")==0)printf("\nYou are working in the -->>> %s logical directory\n",getcwd(pwd, sizeof(pwd)));
+			else if (strcmp(argruments[1],"-P")==0)printf("\nYou are working in the -->>> %s physical directory\n",getcwd(pwd, sizeof(pwd)));
+			else printf("\nError : incorrect option is enterterd\n");
 		}
 
-	else printf("\nYou are working in the -->>> %s directory",getcwd(pwd, sizeof(pwd)));
+	else if(word_count==1)printf("\nYou are working in the -->>> %s directory \n",getcwd(pwd, sizeof(pwd)));
+
+	else printf("\nError: pwd accepts only one argrument\n");
 }
 
 //this function will work for cat command
@@ -194,7 +255,7 @@ void ext_cat(char** address,int word_count)
 	{       
 		if (word_count>3)
 		{
-			printf("cat take atmost 2 argeuments only");
+			printf("\ncat command take atmost 2 argeuments only");
 			exit(0);
 		}
 		else if (word_count>2)
@@ -204,7 +265,7 @@ void ext_cat(char** address,int word_count)
 		}
 		else if (word_count<2)
 		{
-			printf("Please enter second argeument or file name");
+			printf("\nPlease enter second argeument or file name");
 			exit(0);
 		}
 		else
@@ -236,7 +297,7 @@ void ext_date(char** argruments,int word_count)
 	{     
 		if (word_count>3)  
 		{
-			printf("Date take atmost 2 argeuments only");
+			printf("\nDate take atmost 2 argeuments only");
 			exit(0);
 		}
 		else if (word_count>2)
@@ -279,7 +340,7 @@ void ext_ls(char** argruments,int word_count)
 	{       
 		if (word_count>3)
 		{
-			printf("ls accepts only 2 argeuments");
+			printf("\nls accepts only 2 argeuments");
 		}
 		else if (word_count>2)
 		{
@@ -377,7 +438,7 @@ void ext_mkdir(char** argruments,int word_count)
 		}
 		else
 		{
-			printf("Error : Enter file name or option for mkdir");  
+			printf("\nError : Enter file name or option for mkdir");  
         	exit(0);
 		}		
     } 
@@ -390,9 +451,13 @@ void ext_mkdir(char** argruments,int word_count)
 } 
 
 
+
+
+//add_in_history & show_history helps in history function
+//these 2 parameter are for history stroage
 int last_pointer=0;
 char* history[100];
-void add_in_history(char * str)
+void add_in_history(char* str)
 {
 	char *temp;
 	strcpy(temp,str);
@@ -401,11 +466,44 @@ void add_in_history(char * str)
 }
 
 
-void show_history()
+void show_history(char** argruments,int word_count)
 {
-	for (int i=0;i<last_pointer;i++)
+	if (word_count==1)
 	{
-		printf("%i %s\n",i,history[i]);
+		for (int i=0;i<last_pointer;i++)
+		{
+			printf("\n%i %s\n",i+1,history[i]);
+		}
+	}
+	else if (word_count==2)
+	{
+		if (strcmp(argruments[1],"-c")==0)
+		{
+			last_pointer=0;
+			printf("\nHistory cleared\n");
+		}
+		else printf("\nError: Enter a valid option for history\n");
+	}
+
+	else if(word_count==3)
+	{
+		if(strcmp(argruments[1],"-d")==0 && isdigit(argruments[2]))
+		{
+			if (atoi(argruments[2])<=last_pointer+1 && atoi(argruments[2])>1)
+			{
+				printf("\nHistory at %s delected\n",argruments[2]);
+				for(int i=atoi(argruments[2]);i<last_pointer-1;i++)
+				{
+					history[i]=history[i+1];
+				}
+			}
+			else printf("\nError: Enter valid number\n");
+		}
+		else printf("\nError: Enter a valid options for history\n");
+	}
+	else
+	{
+		printf("\nError: History only accepts one argrument or option\n");
 	}
 }
 
@@ -419,12 +517,11 @@ int main()
 
 	while (1) 
 	{ 
-		if (takeInput(input_string)) 
-			continue; 
+		if (takeInput(input_string)==1)continue; 
 
 		add_in_history(input_string);
 
-		Command_handler(processed_string,count_word(input_string, processed_string)); 		
+		Command_handler(processed_string,count_word(input_string, processed_string)); 
 
 	} 
 	return 0; 
